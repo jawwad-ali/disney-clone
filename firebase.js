de03c2a5c96351825ae7ab8a049e2abd0a1f4411
jawwad-ali <alijawwad001@gmail.com>
2021-05-03 22:53:22 +0500

    disney clone first commit

---
+++ firebase.js
@@ -0,0 +1,18 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAoaVdMYdNjdXoB7rxuBnpJpOd5o8Sfg5w",
+  authDomain: "disney-clone-e10ef.firebaseapp.com",
+  projectId: "disney-clone-e10ef",
+  storageBucket: "disney-clone-e10ef.appspot.com",
+  messagingSenderId: "282012857146",
+  appId: "1:282012857146:web:e6e3786498c1d929db1603",
+  measurementId: "G-E7L479BM16",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage();
+
+export { auth, provider, storage };
+export default db;
